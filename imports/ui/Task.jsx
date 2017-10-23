e6a82d0bc8d123dfa97ae6e0ed91748a74e3d00f
Christia Hall <christia@fosterclub.com>
2017-10-23 11:40:41 -0700

    Step 8.9: Update task component to show username

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -33,7 +33,9 @@ render () {
         onClick={this.toggleChecked.bind(this)}
       />
 
-      <span className="text">{this.props.task.text}</span>
+      <span className="text">
+        <strong>{this.props.task.username}</strong> {this.props.task.text}
+      </span>
     </li>
   );
 }
